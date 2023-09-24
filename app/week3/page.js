bf93bfcdf226659dc6f5fd2bb2818547ebcc323e
Felix Cheung <81549914+manho0107@users.noreply.github.com>
2023-09-23 22:00:50 -0600

    commit
    layout changed.

--- app/week3/page.js
+++ app/week3/page.js
@@ -3,11 +3,11 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <main>
-      <h1 className="font-bold text-5xl center-text mb-5">Shopping List</h1>
-      <div>
-        <p>Our shop is designated to provide the best quality of technological devices, from accessories, to laptops, to phones, and more!</p>
-        <p>We accept all cashless payments.</p>
-      </div>
+      <h1 className="font-bold text-5xl center-text mb-5 mt-5 ml-5">Shopping List</h1>
+        <div className="ml-5 mb-3">
+          <p>Our shop is designated to provide the best quality of technological devices, from accessories, to laptops, to phones, and more!</p>
+          <p>We accept all cashless payments.</p>
+        </div>
       <ItemList/>
     </main>
   );
